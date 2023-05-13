7b79c67ca03ed95300f5351379c02721872cb42b
Teacher <teacher@eduhouse.ru>
2023-05-13 14:29:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/viewpager/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 pager = findViewById(R.id.vPager);
+        FragmentStateAdapter pageAdapter = new VpAdapter(this);
+        pager.setAdapter(pageAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/viewpager/VpAdapter.java
@@ -0,0 +1,23 @@
+package com.example.viewpager;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class VpAdapter extends FragmentStateAdapter {
+    public VpAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return PageFragment.newInstance(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+}
